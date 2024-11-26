25a3afbdba1b0c84e28bd992baf4000109b25c11
matheusssantana <matheussilvasantana473@gmail.com>
2024-11-25 22:59:44 -0300

    Update exercicio9.java

--- Lista Vetores/exercicio9.java
+++ Lista Vetores/exercicio9.java
@@ -1,1 +1,20 @@
+public class exercicio9 {
+    public static void main(String[] args) {
+        
+        int[] A = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] B = {5, 6, 7, 8, 11, 12, 13, 14, 15, 16};
 
+       
+        System.out.print("Interseção dos vetores A e B: ");
+        
+        
+        for (int i = 0; i < A.length; i++) {
+            
+            for (int j = 0; j < B.length; j++) {
+                if (A[i] == B[j]) {
+                    System.out.print(A[i] + " ");
+                }
+            }
+        }
+    }
+}
