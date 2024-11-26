2eeddd36193388f7587cba0e592a7b119dedb2e0
matheusssantana <matheussilvasantana473@gmail.com>
2024-11-25 22:27:31 -0300

    Create exercicio3.java

---
+++ Lista While 1/exercicio3.java
@@ -0,0 +1,13 @@
+public class exercicio3 {
+    public static void main(String[] args) {
+        int soma = 0;
+        int n = 0;
+
+        while (n <= 100) {
+            soma= soma+n;
+            n++;
+        }
+
+        System.out.println("A soma dos números de 0 a 100 é: " + soma);
+    }
+}
