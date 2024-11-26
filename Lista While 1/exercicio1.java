939679787534c6e5bb1933b4702abf8fb254e456
matheusssantana <matheussilvasantana473@gmail.com>
2024-11-25 22:25:50 -0300

    Create exercicio1.java

---
+++ Lista While 1/exercicio1.java
@@ -0,0 +1,11 @@
+public class exercicio1 {
+    public static void main(String[] args) {
+        int n = 0;
+        while (n <= 200) {
+            if (n % 2 == 0) {
+                System.out.println(n);
+            }
+            n++;
+        }
+    }
+}
