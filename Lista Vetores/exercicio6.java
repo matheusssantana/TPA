0999ad6bb877cf3465570c5e3e6153e78a2b9ff2
matheusssantana <matheussilvasantana473@gmail.com>
2024-11-25 22:57:45 -0300

    Update exercicio6.java

--- Lista Vetores/exercicio6.java
+++ Lista Vetores/exercicio6.java
@@ -1,1 +1,13 @@
+public class exercicio6 {
+    public static void main(String[] args) {
+        int[] A = new int[11];
 
+        for (int i = 0; i < A.length; i++) {
+            A[i] = (int) Math.pow(2, i);
+        }
+
+        for (int i = 0; i < A.length; i++) {
+            System.out.println("2 elevado à " + i + " =  " + A[i]);
+        }
+    }
+}
