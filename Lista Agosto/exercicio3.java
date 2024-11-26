74695c4239838e230132070b20624547317d6664
matheusssantana <matheussilvasantana473@gmail.com>
2024-11-25 22:38:17 -0300

    Create exercicio3.java

---
+++ Lista Agosto/exercicio3.java
@@ -0,0 +1,18 @@
+
+public class exercicio3 {
+	
+	public static void main (String[] args) {
+		int i = 1;
+		while (i<101) {
+				
+		i = i + (i+1);
+		System.out.println(i);
+		
+			
+			
+			
+			
+		}
+	}
+
+}
