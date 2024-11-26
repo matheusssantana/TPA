25ade2bf3fe7a7257527f10d9a05ebb72f69ed2d
matheusssantana <matheussilvasantana473@gmail.com>
2024-11-25 22:51:45 -0300

    Update exercicio12.java

--- Lista Vetores/exercicio12.java
+++ Lista Vetores/exercicio12.java
@@ -1,1 +1,33 @@
+import java.util.Scanner;
 
+public class exercicio12 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+        int[] A = new int[10];
+
+        System.out.println("Digite 10 números inteiros no vetor:");
+
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Elemento " + i + ": ");
+            A[i] = scanner.nextInt();
+        }
+
+        
+        for (int i = 0; i < A.length / 2; i++) {
+            if (A[i] != A[A.length - 1 - i]) {
+                
+                System.out.println("O vetor NÃO é um palíndromo!");
+                scanner.close();
+                return;
+            }
+        }
+
+        
+        System.out.println("O vetor é um palíndromo!");
+        
+        scanner.close();
+    }
+}
