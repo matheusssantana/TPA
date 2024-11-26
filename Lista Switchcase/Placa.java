c63bcebceb3af4e61590ec8db839750e8e5c5f70
matheusssantana <matheussilvasantana473@gmail.com>
2024-11-25 22:41:01 -0300

    Create Placa.java

---
+++ Lista Switchcase/Placa.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class Placa {
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		int np;
+		System.out.println("Digite o último número da sua placa: ");
+		np = ler.nextInt();
+		
+		switch(np) {
+			
+		case 1:
+		case 2:
+			System.out.println("Segunda-feira");
+		break;
+			
+		case 3:
+		case 4:
+			System.out.println("Terça-feira");
+		break;
+			
+		case 5:
+		case 6:
+			System.out.println("Quarta-feira");
+		break;
+			
+		case 7:
+		case 8:
+			System.out.println("Quinta-feira");
+		break;
+		
+		case 9:
+		case 0:
+			System.out.println("Sexta feira");
+		break;
+		
+		default:
+			System.out.println("Número inválido");
+			
+			
+		}
+	}
+}
