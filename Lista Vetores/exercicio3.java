662c2768a1704323b15e07b67e54926293dd8d4f
matheusssantana <matheussilvasantana473@gmail.com>
2024-11-25 22:55:44 -0300

    Update exercicio3.java

--- Lista Vetores/exercicio3.java
+++ Lista Vetores/exercicio3.java
@@ -1,1 +1,20 @@
+public class exercicio3 {
+    public static void main(String[] args) {
+        int[] A = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
+        for (int i = 0; i < A.length; i++) {
+            int contadorDivisores = 0;
+            for (int j = 1; j <= A[i]; j++) {
+                if (A[i] % j == 0) {
+                    contadorDivisores++;
+                }
+            }
+
+            if (contadorDivisores == 2) {
+                System.out.println(A[i] + " é primo");
+            } else {
+                System.out.println(A[i] + " não é primo");
+            }
+        }
+    }
+}
