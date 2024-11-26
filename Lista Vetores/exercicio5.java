b7e9d31273057e6744ceb7f8f6685edaa74606cd
matheusssantana <matheussilvasantana473@gmail.com>
2024-11-25 22:57:10 -0300

    Update exercicio5.java

--- Lista Vetores/exercicio5.java
+++ Lista Vetores/exercicio5.java
@@ -1,1 +1,13 @@
+public class exercicio5 {
+    public static void main(String[] args) {
+        int[] A = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
+        for (int i = 0; i < A.length; i++) {
+            System.out.print(A[i] + ": ");
+            for (int j = 1; j <= A[i]; j++) {
+                if (A[i] % j == 0) System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
