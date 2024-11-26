a614f184cb0bd3bb6b4ef786b282a8d8e14fba03
matheusssantana <matheussilvasantana473@gmail.com>
2024-11-25 22:52:44 -0300

    Update exercicio13.java

--- Lista Vetores/exercicio13.java
+++ Lista Vetores/exercicio13.java
@@ -1,1 +1,42 @@
+import java.util.Scanner;
 
+public class exercicio13 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        int[] A = new int[10];
+        
+        
+        System.out.println("Digite 10 números inteiros para o vetor A:");
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Elemento " + i + ": ");
+            A[i] = scanner.nextInt();
+        }
+        
+        
+        int[] B = new int[10];
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] % 2 == 0) {
+                B[i] = 1; 
+            } else {
+                B[i] = 0; 
+            }
+        }
+        
+        
+        System.out.print("Vetor A: ");
+        for (int num : A) {
+            System.out.print(num + " ");
+        }
+        
+        System.out.println();
+        
+        System.out.print("Vetor B: ");
+        for (int num : B) {
+            System.out.print(num + " ");
+        }
+
+        scanner.close();
+    }
+}
